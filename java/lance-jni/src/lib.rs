29bbe7ca2ff6d61de2f982054d5b4fbb02d3aadc
Lu Qiu <luqiujob@gmail.com>
2024-02-18 20:59:21 -0800

    Improve WriteOptions: no java defaults, use map

--- java/lance-jni/src/lib.rs
+++ java/lance-jni/src/lib.rs
@@ -14,13 +14,10 @@
 
 use crate::blocking_dataset::BlockingDataset;
 use arrow::ffi_stream::{ArrowArrayStreamReader, FFI_ArrowArrayStream};
-use jni::objects::{JObject, JString};
+use jni::objects::{JMap, JObject, JString};
 use jni::sys::{jint, jlong};
 use jni::JNIEnv;
-use snafu::{location, Location};
-
 use lance::dataset::{WriteMode, WriteParams};
-use lance::Error;
 
 mod blocking_dataset;
 
@@ -30,25 +27,24 @@ pub extern "system" fn Java_com_lancedb_lance_Dataset_writeWithFFIStream<'local>
     _obj: JObject,
     arrow_array_stream_addr: jlong,
     path: JString,
-    max_rows_per_file: jint,
-    max_rows_per_group: jint,
-    max_bytes_per_file: jlong,
-    mode: JString,
+    params: JObject,
 ) -> JObject<'local> {
     let path_str = match extract_path_str(&mut env, &path) {
         Ok(value) => value,
-        Err(_) => return JObject::null(),
+        Err(err_str) => {
+            env.throw_new("java/lang/IllegalArgumentException", err_str)
+                .expect("Error throwing exception");
+            return JObject::null();
+        }
     };
 
-    let write_params = match extract_write_params(
-        &mut env,
-        &max_rows_per_file,
-        &max_rows_per_group,
-        &max_bytes_per_file,
-        &mode,
-    ) {
+    let write_params = match extract_write_params(&mut env, &params) {
         Ok(value) => value,
-        Err(_) => return JObject::null(),
+        Err(err_str) => {
+            env.throw_new("java/lang/IllegalArgumentException", err_str)
+                .expect("Error throwing exception");
+            return JObject::null();
+        }
     };
 
     unsafe {
@@ -114,6 +110,107 @@ fn create_java_dataset_object<'a>(env: &mut JNIEnv<'a>) -> JObject<'a> {
         .expect("Failed to create Java Dataset instance")
 }
 
+fn extract_write_params(env: &mut JNIEnv, params: &JObject) -> Result<WriteParams, String> {
+    let params_map = JMap::from_env(env, params)
+        .map_err(|err| format!("Failed to extract JMap write parameters: {}", err))?;
+
+    let mut write_params = WriteParams::default();
+
+    extract_and_process_jni_map_value::<i32, _>(env, &params_map, "max_rows_per_file", |v| {
+        write_params.max_rows_per_file = v as usize;
+        Ok(())
+    })?;
+
+    extract_and_process_jni_map_value::<i32, _>(env, &params_map, "max_rows_per_group", |v| {
+        write_params.max_rows_per_group = v as usize;
+        Ok(())
+    })?;
+
+    extract_and_process_jni_map_value::<i64, _>(env, &params_map, "max_bytes_per_file", |v| {
+        write_params.max_bytes_per_file = v as usize;
+        Ok(())
+    })?;
+
+    extract_and_process_jni_map_value::<String, _>(env, &params_map, "mode", |mode_str| {
+        match mode_str.to_uppercase().as_str() {
+            "CREATE" => {
+                write_params.mode = WriteMode::Create;
+                Ok(())
+            }
+            "APPEND" => {
+                write_params.mode = WriteMode::Append;
+                Ok(())
+            }
+            "OVERWRITE" => {
+                write_params.mode = WriteMode::Overwrite;
+                Ok(())
+            }
+            _ => Err(format!("Unsupported write mode provided: {}", mode_str)),
+        }
+    })?;
+
+    Ok(write_params)
+}
+
+trait ExtractJniValue {
+    // extract JNI JObject to rust type
+    fn extract(env: &mut JNIEnv, obj: JObject) -> Result<Self, String>
+    where
+        Self: Sized;
+}
+
+impl ExtractJniValue for i32 {
+    fn extract(env: &mut JNIEnv, obj: JObject) -> Result<Self, String> {
+        env.call_method(obj, "intValue", "()I", &[])
+            .and_then(|jvalue| jvalue.i())
+            .map_err(|e| e.to_string())
+    }
+}
+
+impl ExtractJniValue for i64 {
+    fn extract(env: &mut JNIEnv, obj: JObject) -> Result<Self, String> {
+        env.call_method(obj, "longValue", "()J", &[])
+            .and_then(|jvalue| jvalue.j())
+            .map_err(|e| e.to_string())
+    }
+}
+
+impl ExtractJniValue for String {
+    fn extract(env: &mut JNIEnv, obj: JObject) -> Result<Self, String> {
+        env.get_string(&JString::from(obj))
+            .map(|jstr| jstr.into())
+            .map_err(|e| e.to_string())
+    }
+}
+
+fn extract_and_process_jni_map_value<T, F>(
+    env: &mut JNIEnv,
+    map: &JMap,
+    key: &str,
+    mut process: F,
+) -> Result<(), String>
+where
+    T: ExtractJniValue,
+    F: FnMut(T) -> Result<(), String>, // func to apply to return value
+{
+    let key_obj = env
+        .new_string(key)
+        .map(JObject::from)
+        .map_err(|e| e.to_string())?;
+
+    match map.get(env, &key_obj) {
+        Ok(Some(value_obj)) => {
+            if !value_obj.is_null() {
+                let value = T::extract(env, value_obj)?;
+                process(value)?;
+            }
+            Ok(())
+        }
+        Ok(None) => Ok(()), // Key is not present in the map, so we do nothing
+        Err(e) => Err(e.to_string()),
+    }
+}
+
 #[no_mangle]
 pub extern "system" fn Java_com_lancedb_lance_Dataset_open<'local>(
     mut env: JNIEnv<'local>,
@@ -154,68 +251,11 @@ pub extern "system" fn Java_com_lancedb_lance_Dataset_countRows(
     }
 }
 
-fn extract_path_str(env: &mut JNIEnv, path: &JString) -> Result<String, Error> {
+fn extract_path_str(env: &mut JNIEnv, path: &JString) -> Result<String, String> {
     match env.get_string(path) {
         Ok(path) => Ok(path.into()),
-        Err(err) => {
-            env.throw_new(
-                "java/lang/IllegalArgumentException",
-                format!("Invalid path string: {}", err),
-            )
-            .expect("Error throwing exception");
-            Err(Error::InvalidTableLocation {
-                message: format!("Invalid path string: {}", err),
-            })
-        }
+        Err(err) => Err(format!("Invalid path string: {}", err)),
     }
-}
-
-fn extract_write_params(
-    env: &mut JNIEnv,
-    max_rows_per_file: &jint,
-    max_rows_per_group: &jint,
-    max_bytes_per_file: &jlong,
-    mode: &JString,
-) -> Result<WriteParams, Error> {
-    let mode_str: String = match env.get_string(mode) {
-        Ok(mode) => Ok(mode.into()),
-        Err(err) => {
-            env.throw_new(
-                "java/lang/IllegalArgumentException",
-                format!("Invalid mode string: {}", err),
-            )
-            .expect("Error throwing exception");
-            Err(Error::InvalidInput {
-                source: "Invalid mode string".into(),
-                location: location!(),
-            })
-        }
-    }?;
-
-    let mode = match mode_str.as_str() {
-        "CREATE" => WriteMode::Create,
-        "APPEND" => WriteMode::Append,
-        "OVERWRITE" => WriteMode::Overwrite,
-        _ => {
-            env.throw_new(
-                "java/lang/IllegalArgumentException",
-                format!("Unsupported write mode provided: {}", mode_str),
-            )
-            .expect("Error throwing exception");
-            return Err(Error::InvalidInput {
-                source: "Invalid mode string".into(),
-                location: location!(),
-            });
-        }
-    };
-
-    Ok(WriteParams {
-        max_rows_per_file: *max_rows_per_file as usize,
-        max_rows_per_group: *max_rows_per_group as usize,
-        max_bytes_per_file: *max_bytes_per_file as usize,
-        mode,
-        ..Default::default()
-    })
 }
 
 #[no_mangle]
